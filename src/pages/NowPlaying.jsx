0ba07e8b004fd196905334203e6709ac48a094e3
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-16 21:48:24 +0500

    New Commit

--- src/pages/NowPlaying.jsx
+++ src/pages/NowPlaying.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { getRequest } from '../service/getRequest'
+import { API_KEY } from '../hooks/getEnv'
 
 const NowPlaying = () => {
+  const nowPlaying = getRequest(`/now_playing?language=en-US&page=1&key=${API_KEY}`)
+  console.log(nowPlaying)
   return (
     <div>
       NowPlaying
