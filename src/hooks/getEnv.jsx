4d33d66a4f7d9ec9c3437f3290b278c075becf34
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-16 21:43:01 +0500

    New COmmit

---
+++ src/hooks/getEnv.jsx
@@ -0,0 +1,5 @@
+
+
+export const REQUEST = import.meta.env.VITE_API
+export const API_KEY = import.meta.env.VITE_API_KEY
+export const TOKEN = import.meta.env.VITE_TOKEN
---
+++ src/hooks/instance.jsx
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { REQUEST, TOKEN } from "./getEnv";
+
+
+
+export const instance = () => axios.create({baseURL:REQUEST, headers:{Authorization: `Bearer ${TOKEN}`}})
