52633c688cbca8576fbf681403b8cb479c516903
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-17 14:49:05 +0500

    New Commit

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { getRequest } from '../service/getRequest'
+import { API_KEY, IMG_URL } from '../hooks/getEnv'
+
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { BookmarkAdd } from '@mui/icons-material';
+
+
+
+export default function MovieCard({URL}) {
+
+  const data = getRequest(`${URL}?language=en-US&page=1&key=${API_KEY}`)
+  console.log(data)
+  return (
+    <div className='flex flex-wrap objec-cover justify-center items-center gap-2 mt-5'>
+       {data?.results?.map(item => (
+         <Card sx={{ maxWidth: 345 }} key={item.id}>
+         <CardMedia
+           component="img"
+           className='object-cover  w-[100%]'
+           image={`${IMG_URL}/${item.poster_path}`}
+           alt={item.original_title}
+         />
+         <CardContent>
+         <h2 className='text-[20px] font-bold'>
+             {item.original_title}
+           </h2>
+           <Typography className='line-clamp-2' variant="body2" sx={{ color: 'text.secondary' }}>
+              {item.overview}
+           </Typography>
+         </CardContent>
+         <CardActions disableSpacing>
+           <IconButton aria-label="add to favorites">
+             <FavoriteIcon />
+           </IconButton>
+           <IconButton aria-label="share">
+             <BookmarkAdd />
+           </IconButton>
+         </CardActions>
+       </Card>
+       ))}
+    </div>
+  );
+}
+
--- src/pages/NowPlaying.jsx
+++ src/pages/NowPlaying.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import { getRequest } from '../service/getRequest'
-import { API_KEY } from '../hooks/getEnv'
+import MovieCard from '../components/MovieCard'
 
 const NowPlaying = () => {
-  const nowPlaying = getRequest(`/now_playing?language=en-US&page=1&key=${API_KEY}`)
+ 
   return (
-    <div>
-      {nowPlaying?.results?.map(item => (
-        <h2 key={item.id}>{item.original_title}</h2>
-      ))}
-    </div>
+    <MovieCard  URL={`/now_playing`}/>
   )
 }
 
